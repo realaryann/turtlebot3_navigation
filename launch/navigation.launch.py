23fa9df2118b102ed59f4fefc5ef04f014f2611c
aryan <arcnagpal1234@gmail.com>
2024-06-14 11:37:24 -0400

    Added navigation functionality to fork

--- launch/navigation.launch.py
+++ launch/navigation.launch.py
@@ -19,7 +19,7 @@ def generate_launch_description():
     nav2_yaml = os.path.join(get_package_share_directory(
         'turtlebot3_navigation'), 'config', 'amcl_config.yaml')
     map_file = os.path.join(get_package_share_directory(
-        'turtlebot3_navigation'), 'maps', 'DAN409_bf.yaml')
+        'turtlebot3_navigation'), 'maps', 'map.yaml')
     rviz_config_file_path = os.path.join(get_package_share_directory(
         'turtlebot3_navigation'), 'rviz_config', 'pathplanning.rviz')
     waypoint_follower_yaml = os.path.join(get_package_share_directory(
@@ -84,7 +84,10 @@ def generate_launch_description():
             name='waypoint_follower',
             output='screen',
             parameters=[waypoint_follower_yaml]),
-        
+        Node(
+            package = 'turtlebot3_navigation',
+            executable = 'example_node',
+            output = 'screen'),
 
         Node(
             package='nav2_lifecycle_manager',
@@ -98,6 +101,6 @@ def generate_launch_description():
                                         'planner_server',
                                         'behavior_server',
                                         'bt_navigator',
-                                        'waypoint_follower']}])
+                                        'waypoint_follower',]}])
 
     ])
--- turtlebot3_navigation/example.py
+++ turtlebot3_navigation/example.py
@@ -1,39 +1,82 @@
+
+
+import time
+from copy import deepcopy
+
+from geometry_msgs.msg import PoseStamped
+from rclpy.duration import Duration
 import rclpy
-from rclpy.node import Node
-from geometry_msgs.msg import Twist
-from nav_msgs.msg import Odometry
-from sensor_msgs.msg import LaserScan
-from rclpy.qos import ReliabilityPolicy, QoSProfile
 
-class SimplePubSub(Node):
+from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
 
-    def __init__(self):
-        super().__init__('simple_pubsub')
-        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
-        timer_period = 0.5
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.subscriber = self.create_subscription(
-            LaserScan,
-            '/scan',
-            self.listener_callback,
-            QoSProfile(depth=10, reliability=ReliabilityPolicy.RELIABLE))  # is the most used to read LaserScan data and some sensor data.
 
-    def listener_callback(self, msg):
-        self.get_logger().info('I receive: "%s"' % str(msg))
+def main():
+    rclpy.init()
 
-    def timer_callback(self):
-        msg = Twist()
-        msg.linear.x = 0.5
-        msg.angular.z = 0.5
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg)
-            
-def main(args=None):
-    rclpy.init(args=args)
-    simple_pubsub = SimplePubSub()
-    rclpy.spin(simple_pubsub)
-    simple_pubsub.destroy_node()
-    rclpy.shutdown()
+    navigator = BasicNavigator()
+
+    # Inspection route, probably read in from a file for a real application
+    # from either a map or drive and repeat.
+    inspection_route = [
+        [0.533665, -0.544118],
+        [4.27509, 5.92226],
+        [2.28073, 8.95063],
+        [-3.29666, 5.04603]]
+
+    # Set your demo's initial pose
+    initial_pose = PoseStamped()
+    initial_pose.header.frame_id = 'map'
+    initial_pose.header.stamp = navigator.get_clock().now().to_msg()
+    initial_pose.pose.position.x = 0
+    initial_pose.pose.position.y = 0
+    initial_pose.pose.orientation.z = 0.816279
+    initial_pose.pose.orientation.w = 0.577657
+    navigator.setInitialPose(initial_pose)
+
+    # Wait for navigation to activate fully 
+    navigator.waitUntilNav2Active()
+
+    # Send your route
+    inspection_points = []
+    inspection_pose = PoseStamped()
+    inspection_pose.header.frame_id = 'map'
+    inspection_pose.header.stamp = navigator.get_clock().now().to_msg()
+    inspection_pose.pose.orientation.z = 1.0
+    inspection_pose.pose.orientation.w = 0.0
+    for pt in inspection_route:
+        inspection_pose.pose.position.x = pt[0]
+        inspection_pose.pose.position.y = pt[1]
+        inspection_points.append(deepcopy(inspection_pose))
+    nav_start = navigator.get_clock().now()
+    navigator.followWaypoints(inspection_points)
+
+    # Do something during your route (e.x. AI to analyze stock information or upload to the cloud)
+    # Print the current waypoint ID for the demonstration
+    i = 0
+    while not navigator.isTaskComplete():
+        i = i + 1
+        feedback = navigator.getFeedback()
+        if feedback and i % 5 == 0:
+            print('Executing current waypoint: ' +
+                  str(feedback.current_waypoint + 1) + '/' + str(len(inspection_points)))
+
+    result = navigator.getResult()
+    if result == TaskResult.SUCCEEDED:
+        print('Inspection of shelves complete! Returning to start...')
+    elif result == TaskResult.CANCELED:
+        print('Inspection of shelving was canceled. Returning to start...')
+        exit(1)
+    elif result == TaskResult.FAILED:
+        print('Inspection of shelving failed! Returning to start...')
+
+    # go back to start
+    initial_pose.header.stamp = navigator.get_clock().now().to_msg()
+    navigator.goToPose(initial_pose)
+    while not navigator.isTaskComplete():
+        pass
+
+    exit(0)
+
 
 if __name__ == '__main__':
     main()
